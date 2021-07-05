1fd02aa5c8679f95e2939b06251ec52e36db2e94
David Rambo <davrambo@gmail.com>
2021-07-05 14:13:41 -0700

    Got super agenda groups to work.

--- config.el
+++ config.el
@@ -16,16 +16,6 @@
         doom-themes-enable-italic t
         doom-gruvbox-light-variant "hard"))
 
-(setq company-global-modes '(not org-mode))
-
-(setq display-line-numbers-type t)
-
-;(global-linum-mode t)
-;(setq linum-format "%2d ")
-
-(add-hook 'org-mode-hook (lambda ()
-            (setq hl-line-mode nil)))
-
 (after! org
  (add-hook 'org-mode-hook 'org-indent-mode)
  (setq org-directory "~/notes/"
@@ -33,26 +23,10 @@
        org-agenda-files '("~/notes/tasks.org")
        org-hide-emphasis-markers t)
 
-;; TODOs
-  (setq org-todo-keywords
-        (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
-                (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)" "MEETING"))))
-
-  (use-package! org-superstar-mode
-    :custom
-    org-superstar-headline-bullets-list '("◉" "○" "⁖" "◌" "◿")
-    org-superstar-first-inlinetask-bullet '("▷")
- ;   org-superstar-item-bullet-alist '("•")
-    :hook (org-mode . org-bullets-mode))
-
  (require 'org-inlinetask) ; C-c C-x t
 
  ;; Turn off quote block styling by toggling
  (setq org-fontify-quote-and-verse-blocks 'nil)
-
-; (font-lock-add-keywords 'org-mode
-;                          '(("^ *\\([-+*]\\) "
-;                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "◦"))))))
 
  (let* ((variable-tuple
           (cond ((x-list-fonts "Source Sans Pro") '(:font "Source Sans Pro"))
@@ -67,19 +41,17 @@
      `(org-level-7 ((t (,@headline ,@variable-tuple))))
      `(org-level-6 ((t (,@headline ,@variable-tuple))))
      `(org-level-5 ((t (,@headline ,@variable-tuple))))
-     `(org-level-4 ((t (,@headline ,@variable-tuple :size 14))))
+     `(org-level-4 ((t (,@headline ,@variable-tuple))))
      `(org-level-3 ((t (,@headline ,@variable-tuple :size 14))))
-     `(org-level-2 ((t (,@headline ,@variable-tuple :size 16))))
-     `(org-level-1 ((t (,@headline ,@variable-tuple :size 18))))
-     `(org-document-title ((t (,@headline ,@variable-tuple :height 1.25 :underline nil))))))
+     `(org-level-2 ((t (,@headline ,@variable-tuple :size 14))))
+     `(org-level-1 ((t (,@headline ,@variable-tuple :size 16))))
+     `(org-document-title ((t (,@headline ,@variable-tuple :height 1.1 :underline nil))))))
 
  (custom-theme-set-faces
   'user
   ;'(variable-pitch ((t (:family "ETBembo" :height 160 :weight thin))))
   '(variable-pitch ((t (:family "Source Sans Pro" :size 14 :weight regular))))
   '(fixed-pitch ((t ( :family "Ubuntu Mono" )))))
-
-
 
  (add-hook 'org-mode-hook 'variable-pitch-mode)
 
@@ -96,9 +68,41 @@
    '(org-property-value ((t (:inherit fixed-pitch))) t)
    '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
    '(org-table ((t (:inherit fixed-pitch :foreground "#83a598"))))
-   '(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
-   '(org-verbatim ((t (:inherit (shadow fixed-pitch))))))
+   '(org-tag ((t (:inherit (shadow fixed-pitch) :weight regular :height 0.8))))
+   '(org-verbatim ((t (:inherit (shadow fixed-pitch)))))
+)
+
 ) ; end after! org
+
+(setq company-global-modes '(not org-mode))
+
+(setq display-line-numbers-type nil)
+
+;(global-linum-mode t)
+;(setq linum-format "%2d ")
+
+;(add-hook 'org-mode-hook (lambda ()
+;            (setq hl-line-mode nil)))
+
+(setq org-todo-keywords
+      (quote ((sequence "TODO(t)" "NEXT(n)" "IN-PROGRESS(p)" "|" "DONE(d)")
+              (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)" "MEETING"))))
+
+(after! org
+(setq org-todo-keyword-faces
+ '(("TODO" . (:foreground "#DC322F" :weight regular))
+   ("NEXT" . (:foreground "#6C71C4" :weight bold))
+   ("IN-PROGRESS" . (:foreground "#2AA198" :weight bold))
+   ("DONE" . (:foreground "#002B36" :weight light))
+ ))
+)
+
+(use-package! org-superstar-mode
+    :custom
+    org-superstar-headline-bullets-list '("◉" "○" "⁖" "◌" "◿")
+    org-superstar-first-inlinetask-bullet '("▷")
+ ;   org-superstar-item-bullet-alist '("•")
+    :hook (org-mode . org-bullets-mode))
 
 (setq deft-directory "~/notes/"
       deft-extensions '("org")
@@ -113,33 +117,65 @@
         org-journal-date-format "%B %d, %Y (%A) "
         org-journal-file-format "%Y-%m-%d.org")
 
-  (setq org-journal-enable-agenda-integration t)
-)
-
-(use-package! org-super-agenda
-  :commands (org-super-agenda-mode)
-  :after org-agenda
-  :init
-  (setq
-        org-log-done 'time
-        org-agenda-skip-scheduled-if-done t
-        org-agenda-skip-deadline-if-done t
-        org-agenda-include-deadlines t
-        org-agenda-block-separator nil
-        org-agenda-tags-column 100
-        org-agenda-compact-blocks t)
-  :config
-  (org-super-agenda-mode)
+  (setq org-journal-enable-agenda-integration nil)
 )
 
 (setq org-agenda-custom-commands
       '(("n" "Agenda and all TODOs"
          ((agenda "")
          (alltodo "")))
-      ("h" "Home-related tasks" tags-todo "home"
-         ((org-agenda-files '("~/notes/tasks.org")))
-         )
-      ("w" "Work-related tasks" tags-todo "postdoc|book"))
+        ("h" "Home-related tasks" tags-todo "home"
+           ((org-agenda-files '("~/notes/tasks.org"))) ; For when I expand agenda files and want this to be quick.
+           )
+        ("w" "Work-related tasks" tags-todo "postdoc|book")
+        ("b" "Book-related tasks" tags-todo "book"))
+)
+
+(use-package! org-super-agenda
+  :after org-agenda
+  :init
+  (setq
+        org-log-done nil
+        org-agenda-skip-scheduled-if-done t
+        org-agenda-skip-deadline-if-done t
+        org-agenda-include-deadlines t
+        org-agenda-block-separator 9472
+        org-agenda-tags-column 100
+        org-agenda-compact-blocks nil)
+  :config
+  (org-super-agenda-mode)
+)
+
+(setq org-super-agenda-groups
+      '((:name "Today"
+         :time-grid t
+         :scheduled today
+         :deadline today
+         :face (:foreground "#DC322F")
+         :order 1)
+        (:name "In Progress"
+         :todo ("IN-PROGRESS")
+         :face (:foreground "#2AA198")
+         :order 2)
+        (:name "Next"
+         :todo ("NEXT")
+         :face (:foreground "#6C71C4")
+         :order 3)
+        (:order-multi (2 (:name "Work"
+                          :and (:tag "postdoc"))
+                         (:name "Reading"
+                          :and (:tag "reading")
+                          :not (:tag "postdoc"))
+                         (:name "Writing"
+                          :and (:tag "book" :tag "writing")
+                          :not (:tag "reading"))
+                      )
+        )
+         (:name "Remaining Tasks"
+                :and (:todo "TODO"
+                      :not (:todo "postdoc" :todo "IN-PROGRESS" :todo "NEXT" :todo "reading" :todo "writing")))
+         (:todo "WAITING" :order 8)
+       )
 )
 
 (define-key evil-normal-state-map (kbd "<remap> <evil-next-line>") 'evil-next-visual-line)
@@ -164,6 +200,12 @@
   (interactive)
   (find-file-existing "~/notes/tasks.org"))
 (global-set-key (kbd "C-c t") 'open-task-file)
+
+(defun open-hours-log ()
+  "Open hours-log.org file."
+  (interactive)
+  (find-file-existing "~/notes/hours-log.org"))
+(global-set-key (kbd "C-c h") 'open-hours-log)
 
 ;(defun open-journal-entry ()
 ;  "Open today's journal entry."
