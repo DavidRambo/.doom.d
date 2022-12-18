27d4bb13e9680b0c4f0b78dd6f3dcf95d08b050b
David Rambo <davidrambo@mailfence.com>
2022-12-17 22:03:37 -0800

    Add doom profiles.el

---
+++ profiles.el
@@ -0,0 +1,5 @@
+;; ~/.config/doom/profiles.el
+((default   (user-emacs-directory . "~/.config/emacs/")
+            ; ("DOOMDIR" . "~/.config/doom/"))
+            ("DOOMDIR" . "~/.doom.d/"))
+ (efs       (user-emacs-directory . "~/.config/emacs-from-scratch/")))
